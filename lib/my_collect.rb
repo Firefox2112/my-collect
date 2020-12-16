4da0a2de42d523449e0e7c6255c0e2b883442d88
Coby <cobyrpilpel2@gmail.com>
2020-12-15 21:02:56 -0500

    commit tree 976e34c44b08d5224cf3f7fa2c859f0afe736958

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+def my_collect(collecttest)
+    i = 0
+    collection = []
+    while i < collecttest.length
+        collection << yield(collecttest[i])
+        i+=1
+    end
+    collection
+end
 
-
+## Arrays as primary focus of "Why does this work" instead of just accepting that it works. 
